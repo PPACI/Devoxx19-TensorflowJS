6951da34761c9afb4c6b161233ea6c7b5124b84c
pierre paci <pierre.paci@gmail.com>
2019-03-02 14:30:28 +0100

    image input reader

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,17 +11,44 @@ const styles = {
     root: {
         flexGrow: 1,
     },
-    demo:{
+    demo: {
         height: '800px'
     },
-    paper:{
-      width: '300px',
-      height: '300px'
+    paper: {
+        width: '300px',
+        height: '300px'
+    },
+    imageInput: {
+        maxWidth: "300px",
+        maxHeight: "270px"
     }
 };
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageUrl: null
+        };
+
+        this.handleFileChange = this.handleFileChange.bind(this);
+    }
+
+    handleFileChange(e) {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file = e.target.files[0];
+
+        reader.onloadend = () => {
+            this.setState({
+                imageUrl: reader.result
+            });
+        };
+
+        reader.readAsDataURL(file)
+    }
+
     render() {
         const {classes} = this.props;
         return (
@@ -34,12 +61,13 @@ class App extends Component {
                         </Typography>
                     </AppBar>
                     <Grid container className={classes.demo} justify="space-evenly" alignItems="center">
-                        <Grid item xs="{6}">
+                        <Grid item xs={6}>
                             <Paper className={classes.paper}>
-                                Image
+                                <img src={this.state.imageUrl} alt="input" className={classes.imageInput}/>
+                                <input type="file" onChange={this.handleFileChange}/>
                             </Paper>
                         </Grid>
-                        <Grid item xs="{4}">
+                        <Grid item xs={4}>
                             <Paper className={classes.paper}>
                                 Prediction
                             </Paper>
