264034770497ba36dc62fcfa0121713ba457ee82
pierre paci <pierre.paci@gmail.com>
2019-04-08 12:01:53 +0200

    inference from webcam

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,39 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {AppBar, CssBaseline, Grid, Paper, Toolbar, Typography} from '@material-ui/core';
-import {CloudUpload} from '@material-ui/icons'
+import React, { useEffect, useRef, useState } from 'react';
+import { AppBar, CssBaseline, Grid, Paper, Toolbar, Typography } from '@material-ui/core';
+import { CloudUpload } from '@material-ui/icons'
 import * as tf from "@tensorflow/tfjs";
 import Fab from "@material-ui/core/es/Fab/Fab";
 import styles from "./App.css";
 
 
 export default function App() {
-    const [model, setModel] = useState(null);
     const [results, setResults] = useState(null);
     const [image, setImage] = useState(`${process.env.PUBLIC_URL}devoxx.png`);
-    const fileInputEl = useRef(null);
+    const webcamEl = useRef(null);
 
     const loadModel = async () => {
         try {
-            setModel(await tf.loadLayersModel(`${process.env.PUBLIC_URL}/model/model.json`));
+            return await tf.loadLayersModel(`${process.env.PUBLIC_URL}/model/model.json`);
         } catch (err) {
             console.error(err);
         }
     };
 
     useEffect(() => {
-        loadModel();
+        loadModel().then(model => {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(webcamStream => {
+                    webcamEl.current.ontimeupdate = event => predict(event, model);
+                    webcamEl.current.srcObject = webcamStream
+                }).catch(console.error)
+        });
     }, []);
 
-    const predict = async image => {
+    async function predict(event, model) {
+        const { target } = event
+        console.log(target)
         // Actual prediction
-        const input = tf.browser.fromPixels(image)
+        const input = tf.browser.fromPixels(target)
             .resizeBilinear([224, 224])
             .expandDims(0);
         input.print();
@@ -34,34 +41,8 @@ export default function App() {
         setResults(await prediction.data());
     };
 
-    const handleFileChange = e => {
-        e.preventDefault();
-        const reader = new FileReader();
-        const file = e.target.files[0];
-
-        reader.onloadend = () => {
-            // add preview
-            setImage(reader.result);
-
-            // construct a new Image object for tf.js
-            const image = new Image();
-            image.onload = () => {
-                predict(image)
-            };
-
-            image.src = reader.result;
-        };
-
-        reader.readAsDataURL(file)
-    };
-
-    const chooseFile = () => {
-        fileInputEl.current.click();
-    };
-
-
     const resultsDescription = () => {
-        if (results === null){
+        if (results === null) {
             return 'Prediction'
         } else if (results[0] > 0.5) {
             return `Croissant : ${results[0]}`
@@ -73,7 +54,7 @@ export default function App() {
 
     return (
         <>
-            <CssBaseline/>
+            <CssBaseline />
             <div className="root">
                 <AppBar position="static">
                     <Toolbar>
@@ -85,13 +66,13 @@ export default function App() {
                 <Grid container justify="space-evenly" alignItems="center" >
                     <Grid item xs={12} sm={10} md={8} lg={6}>
                         <Paper className={styles.root}>
-                            <div className={styles.container}>
-                                <img src={image} alt="input" className={styles.image}/>
-                                <Fab color="primary" aria-label="Edit" className={styles.fab} onClick={chooseFile}>
-                                    <CloudUpload />
-                                </Fab>
-                            </div>
-                            <input type="file" id="file" ref={fileInputEl} className={styles.fileInput} onChange={handleFileChange}/>
+                            <Grid container justify="space-evenly" alignItems="center" >
+                                <Grid item xs={12} sm={10} md={8} lg={6}>
+                                    <div className={styles.container}>
+                                        <video id="v1" autoPlay width="500" height="500" ref={webcamEl}></video>
+                                    </div>
+                                </Grid>
+                            </Grid>
                             {resultsDescription()}
                         </Paper>
                     </Grid>
